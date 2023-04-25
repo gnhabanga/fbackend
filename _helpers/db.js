98d6426f4de61b07f780c774b2d5d06a73e84f93
Gladistone Nhabanga <nhabanga.gladston@gmail.com>
2023-04-25 08:53:59 +0000

    Update 3 files

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,9 +1,32 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
-const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
-mongoose.Promise = global.Promise;
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your MongoDB connection string
+const uri =
+  "mongodb+srv://gladston:Gladston2@cluster0.jyxsh90.mongodb.net/firstDB?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+// Connect to the client
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // If connection is successful
+  console.log("Connected to MongoDB");
+
+  // Get a reference to the database
+  const db = client.db("firstDB");
+
+  // Perform database operations
+  // ...
+
+  // Close the connection when finished
+  client.close();
+});
 
 module.exports = {
-    User: require('../users/user.model')
+  User: require("../users/user.model"),
 };
